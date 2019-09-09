fd6e7cfd2bd6f2f17e4ce32ff052df9c6cb08be2
wesley <truthotwo@gmail.com>
2019-09-10 00:29:12 +0900

    movie web structure setting

--- src/App.js
+++ src/App.js
@@ -4,33 +4,19 @@ import React from 'react';
 class App extends React.Component {
 
   state = {
-    count: 0
-  }
-
-  plus = () => {
-    this.setState(current => ({ count: current.count + 1 }))
-  }
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }))
+    isLoaded: true
   }
 
   componentDidMount() {
-    console.log("rendering completed")
-  }
-
-  componentDidUpdate() {
-    console.log("updated")
+    setTimeout(() => {
+      this.setState({ isLoaded: false })
+    }, 3000)
   }
 
   render() {
-    console.log("rendering...")
+    const { isLoaded } = this.state
     return (
-      <div>
-        <h1>Number : {this.state.count}</h1>
-        <button onClick={this.plus}>+</button>
-        <button onClick={this.minus}>-</button>
-      </div>
+      <div>{isLoaded ? "Loading" : "Contents"}</div>
     )
   }
 }
